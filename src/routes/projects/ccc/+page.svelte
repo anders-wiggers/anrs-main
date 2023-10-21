f6cae466dfd1ab98bbcdff7a4850bca219e56579
anders-wiggers <contact@anrs.dk>
2023-10-22 00:14:10 +0200

    added CCC page

--- src/routes/projects/ccc/+page.svelte
+++ src/routes/projects/ccc/+page.svelte
@@ -1,4 +1,92 @@
+<script>
+	import { FileDropzone } from '@skeletonlabs/skeleton';
+	import { ProgressBar } from '@skeletonlabs/skeleton';
+
+	let files;
+
+	let fileName = 'Upload a Image';
+
+	function onChangeHandler(e) {
+		console.log('file data:', e);
+		console.log('file data:', files);
+		fileName = files[0].name;
+	}
+
+	let exampleJson = [
+		{
+			Santaclause: 7
+		}
+	];
+</script>
+
 <svelte:head>
 	<title>Check Christmas Calenders</title>
 	<meta name="description" content="Christmas calenders checker" />
 </svelte:head>
+
+<h1 class="h1">Christmas Calendar Checker</h1>
+<div class="mt-10">
+	The <code>Christmas Calendar Checker</code> project employs deep learning techniques to identify icons
+	on the Danish Quick Christmas Calder game. The model was trained utilizing transfer learning and a
+	dataset of annotated images collected during the Christmas season. The model can be accessed via a
+	Python-based flash API, which enables users to submit an image and receive the results in JSON format.
+	The project and an accompanying demonstration video can be accessed on my personal Github page.
+</div>
+<div class="card m-4 p-6">
+	<div class="flex">
+		<h2 class="h2">Check a Calender</h2>
+		<div class="flex items-center">
+			<span class="chip variant-filled ml-5 cursor-default"
+				>Feature will unlock at Christmas {new Date().getFullYear()}</span
+			>
+		</div>
+	</div>
+
+	<p>Simply take a photo of your calender and drop it onto the field below</p>
+	<div class="mt-10">
+		<FileDropzone bind:files name="files" on:change={onChangeHandler} disabled>
+			<svelte:fragment slot="lead"><i class="fa fa-upload" /></svelte:fragment>
+			<svelte:fragment slot="message">{fileName}</svelte:fragment>
+			<svelte:fragment slot="meta">PNG and JPG are allowed</svelte:fragment>
+		</FileDropzone>
+	</div>
+	<div class="pt-2 flex justify-end">
+		<button disabled type="button" class="btn bg-gradient-to-br variant-gradient-primary-secondary">
+			<span><i class="fa fa-upload" /></span>
+			<span>Submit Photo</span>
+		</button>
+	</div>
+
+	<hr class="mt-10" />
+
+	<div class="mt-10">
+		<div class="flex">
+			<h3 class="h3">Results</h3>
+			<div class="flex items-center">
+				<span class="ml-5 cursor-default italic">Example of results</span>
+			</div>
+		</div>
+		<div class="mt-5">
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Symbol</th>
+						<th>Amount</th>
+						<th>Progress</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td> Santaclause </td>
+						<td> <b>7</b>/<b>10</b></td>
+						<td>
+							<div class="flex">
+								<ProgressBar value={70} />
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
--- src/routes/projects/tda/+page.svelte
+++ src/routes/projects/tda/+page.svelte
@@ -2,3 +2,51 @@
 	<title>Tour de Azeroth</title>
 	<meta name="description" content="The tour of Azeroth" />
 </svelte:head>
+<div>
+	<h1 class="h1">Tour De Azeroth</h1>
+
+	<div class="mt-10">
+		Welcome to the extraordinary world of Tour de Azeroth - a unique event where heroes embark on a
+		daring footrace through the vast landscapes of Azeroth and its corresponding outlands! For both
+		seasoned adventurers and newcomers to the World of Warcraft, Tour de Azeroth offers an
+		exhilarating showcase of endurance, bravery, and determination that will leave you in awe.
+		<br />
+		<br />
+		Originally conceived to celebrate the spirit of exploration and competition, Tour de Azeroth has
+		become an annual tradition cherished by the denizens of this wondrous realm. The race leads participants
+		on a challenging journey through Azeroth's diverse landscapes, from tranquil forests and majestic
+		mountains to scorching deserts and enchanting cities.
+		<br />
+		<br />
+		While the race revolves around individual achievements, community is an integral part of Tour de
+		Azeroth. Participants often form impromptu teams, supporting and encouraging each other through the
+		grueling journey. Bonds are forged in shared trials and triumphs, fostering a sense of unity among
+		the runners.
+
+		<br />
+		<br />
+		At the heart of Tour de Azeroth lies the pursuit of the prestigious "Azerothian Yellow Boots," a
+		symbolic prize awarded to the race leader. These coveted boots signify not only athletic prowess
+		but also the spirit of adventure and determination that characterizes the champions of Azeroth.
+
+		<br />
+		<br />
+		More than just a race, Tour de Azeroth celebrates the vibrant culture and diverse races of this incredible
+		world. As characters traverse the continents, they are treated to breathtaking vistas, ancient ruins,
+		and hidden wonders that showcase the vastness and beauty of Azeroth.
+
+		<br />
+		<br />
+		So lace up your running shoes, summon your courage, and join us on this thrilling adventure as we
+		embark on Tour de Azeroth! Whether you're a seasoned adventurer or a curious novice, this extraordinary
+		race promises an unforgettable experience filled with heroism, camaraderie, and the unwavering spirit
+		of Azeroth's inhabitants. Come and join us, let the grand journey begin!
+
+		<div class="mt-10 flex justify-center">
+			<a href="https://tda.anrs.dk/" class="btn variant-filled">
+				<span />
+				<span>Visit Project Homepage</span>
+			</a>
+		</div>
+	</div>
+</div>
